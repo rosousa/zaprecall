98384b5aa0b1f7bf94d9ef8663e3a299c7562677
Rodolfo Sousa <rodolfozsousa@gmail.com>
2022-07-22 23:44:34 -0300

    Added More Cards

--- src/components/CardScreen.js
+++ src/components/CardScreen.js
@@ -16,6 +16,23 @@ function CardScreen({ res, setRes }) {
     },
     { question: "Componentes devem iniciar com __", answer: "letra maiúscula" },
     { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    {
+      question: "O ReactDOM nos ajuda __",
+      answer: "interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      question: "Usamos o npm para __",
+      answer: "gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      question: "Usamos props para __",
+      answer: "passar diferentes informações para componentes",
+    },
+    {
+      question: "Usamos estado (state) para __",
+      answer:
+        "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    },
   ].sort(() => Math.random() - 0.5);
 
   function Result() {
